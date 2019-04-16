632c3dac2616647b7e8c3ed32c2b6aa8974b3e0c
Paige Wiener <snowplushie@yahoo.com>
2019-04-16 23:24:58 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,14 @@ class Game
   attr_accessor :board, :player_1, :player_2
   
    WIN_COMBINATIONS = [
-   [0,1,2],
-   [3,4,5],
-   [6,7,8],
-   [0,3,6],
-   [1,4,7],
-   [2,5,8],
-   [0,4,8],
-   [2,4,6]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
  ]
  
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
@@ -35,7 +35,7 @@ class Game
   end
   
   def over?
-    @board.full? || won?
+    draw? || won?
   end
   
   def winner
