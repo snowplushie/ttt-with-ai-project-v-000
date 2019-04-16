4b424a5df106e8e11bd4e4d631ec4eb08d9b1005
Paige Wiener <snowplushie@yahoo.com>
2019-04-16 22:40:42 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -24,9 +24,9 @@ class Game
   
   def won?
     WIN_COMBINATIONS.detect do |combination|
-      @board[combination[0]] == board[combination[1]] &&
-      @board[combination[1]] == board[combination[2]] &&
-      @board.taken?(board, combination[0])
+      @board.cells[combination[0]] == @board.cells[combination[1]] &&
+      @board.cells[combination[1]] == @board.cells[combination[2]] &&
+      @board.taken?(combination[0])
     end
   end
 end
