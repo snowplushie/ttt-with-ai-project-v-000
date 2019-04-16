fcbdda8fdb3485d188d447dd80d0572c3a13f093
Paige Wiener <snowplushie@yahoo.com>
2019-04-16 22:03:05 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -12,8 +12,8 @@ class Board
   def display
     puts "#{cell[0]}|#{cell[1]}|#{cell[2]}"
     puts "-----------"
-    puts "   |   |   "
+    puts "#{cell[3]}|#{cell[4]}|#{cell[5]}"
     puts "-----------"
-    puts "   |   |   "
+    puts "#{cell[6]}|#{cell[7]}|#{cell[8]}"
   end
 end
