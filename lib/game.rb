e22af008ba127a43c5cab69cb9aab8e0513346cc
Paige Wiener <snowplushie@yahoo.com>
2019-04-16 23:05:06 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -39,8 +39,8 @@ class Game
   end
   
   def winner
-    if winning_combination = won?
-      @winner = @board.cells[winning_combination.first]
+    if winning_combo = won?
+      @winner = @board.cells[winning_combo.first]
     end
   end
   
